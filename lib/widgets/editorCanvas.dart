31fab585f06bc84716f9507dec8afac4b8aafa17
wyswwill <wyswill4290>
2020-06-15 08:51:32 +0800

    波形绘制

--- lib/widgets/editorCanvas.dart
+++ lib/widgets/editorCanvas.dart
@@ -17,15 +17,17 @@ class EditorCanvas extends CustomPainter {
 
     /// 挨个画频谱柱子
     double left = 0;
+    //print(canvasData);
     for (int i = 0; i < canvasData.length; i++) {
       double volume = 2.0;
+
       List<int> current = canvasData[i];
       volume = (current[0] + current[1]) * 0.01;
       if (volume > size.height) volume = size.height - 250;
-      double heigh = (size.height - volume) / 2;
-
+      double low =  0.5 - (current[1])/(65536.0 * 1.5);
+      double h = (current[1]-current[0])/(65536.0*1.5);
       ///柱子
-      Rect column = Rect.fromLTWH(left, heigh, columnWidth, volume);
+      Rect column = Rect.fromLTWH(left, low * size.height, columnWidth, h * size.height);
       left += 1;
 
       ///波形柱子
